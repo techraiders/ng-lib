4b98207571e25618f5463fa7c93af22bdf11621c
Navneet Prakash <hello_navneet@hotmail.com>
2025-04-20 12:56:05 +0530

    Generates my-library 	modified:   angular.json 	modified:   package-lock.json 	modified:   package.json 	new file:   projects/my-library/README.md 	new file:   projects/my-library/ng-package.json 	new file:   projects/my-library/package.json 	new file:   projects/my-library/src/lib/my-library.component.spec.ts 	new file:   projects/my-library/src/lib/my-library.component.ts 	new file:   projects/my-library/src/lib/my-library.service.spec.ts 	new file:   projects/my-library/src/lib/my-library.service.ts 	new file:   projects/my-library/src/public-api.ts 	new file:   projects/my-library/tsconfig.lib.json 	new file:   projects/my-library/tsconfig.lib.prod.json 	new file:   projects/my-library/tsconfig.spec.json 	modified:   tsconfig.json

---
+++ projects/my-library/src/lib/my-library.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-my-library',
+  imports: [],
+  template: `
+    <p>
+      my-library works!
+    </p>
+  `,
+  styles: ``
+})
+export class MyLibraryComponent {
+
+}
